e214ad76026ad50e16b1b0c75a29e23d84cfa8df
StarbugMolt <starbugmolt@gmail.com>
2026-02-15 20:29:46 +0100

    Fix: Correct router import paths

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from './views/Home.vue'
-import About from './views/About.vue'
-import Projects from './views/Projects.vue'
+import Home from '../views/Home.vue'
+import About from '../views/About.vue'
+import Projects from '../views/Projects.vue'
 
 const routes = [
   { path: '/', name: 'Home', component: Home },
